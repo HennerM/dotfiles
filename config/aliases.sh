95b754360b6e1aa1d31120cc5400195e56eb8fdd
Markus Hennerbichler <markushennerbichler@gmail.com>
2025-10-09 10:40:29 +0200

    fix df alias for macos

--- config/aliases.sh
+++ config/aliases.sh
@@ -30,7 +30,7 @@ alias le="less -N"
 
 # storage
 alias du='du -kh' # file space
-alias df='df -kTh' # disk space
+alias df='df -kh' # disk space
 alias usage='du -sh * 2>/dev/null | sort -rh'
 alias dus='du -sckx * | sort -nr'
 #
